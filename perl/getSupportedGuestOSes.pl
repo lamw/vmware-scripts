1bbecea5db34ee3ac68469015872e47bad1bcbc4
William Lam <william2003@gmail.com>
2013-12-10 22:52:26 -0800

    Adding supported guestOS & virtual hw version

---
+++ perl/getSupportedGuestOSes.pl
@@ -0,0 +1,52 @@
+#!/usr/bin/perl -w
+# William Lam
+# http://blogs.vmware.com/vsphere/automation
+
+use strict;
+use warnings;
+use VMware::VILib;
+use VMware::VIRuntime;
+
+my %opts = (
+        cluster => {
+        type => "=s",
+        help => "The name of a vCenter cluster to query",
+        required => 1,
+        },
+);
+
+# validate options, and connect to the server
+Opts::add_options(%opts);
+Opts::parse();
+Opts::validate();
+Util::connect();
+
+my $cluster_name = Opts::get_option('cluster');
+
+my $cluster_view = Vim::find_entity_view(view_type => 'ClusterComputeResource', filter => { name => $cluster_name });
+
+unless (defined $cluster_view){
+        print "No cluster found with name $cluster_name!\n";
+	Util::disconnect();
+	exit 1;
+}
+
+my $hosts = Vim::get_views(mo_ref_array => $cluster_view->host, properties => ['name']);
+
+print "Host: " . @$hosts[0]->{'name'} . "\n\n";
+
+my $envBrowser = Vim::get_view(mo_ref => $cluster_view->environmentBrowser);
+
+eval {
+	my $vmOptions = $envBrowser->QueryConfigOption(host => @$hosts[0]);
+
+	my $guestOSes = $vmOptions->guestOSDescriptor;
+	foreach my $guest (sort {$a->fullName cmp $b->fullName}@$guestOSes) {
+		print $guest->id . " -- " . $guest->fullName . "\n";
+	}
+};
+if($@) {
+	print "Error: " . $@ . "\n";
+}
+
+Util::disconnect();
---
+++ perl/getSupportedVirtualHardwareVersion.pl
@@ -0,0 +1,48 @@
+#!/usr/bin/perl -w
+# William Lam
+# http://blogs.vmware.com/vsphere/automation
+
+use strict;
+use warnings;
+use VMware::VILib;
+use VMware::VIRuntime;
+
+my %opts = (
+        cluster => {
+        type => "=s",
+        help => "The name of a vCenter cluster",
+        required => 1,
+        },
+);
+
+# validate options, and connect to the server
+Opts::add_options(%opts);
+Opts::parse();
+Opts::validate();
+Util::connect();
+
+my $cluster_name = Opts::get_option('cluster');
+
+my $cluster_view = Vim::find_entity_view(view_type => 'ClusterComputeResource', filter => { name => $cluster_name });
+
+unless (defined $cluster_view){
+        print "No cluster found with name $cluster_name!\n";
+	Util::disconnect();
+	exit 1;
+}
+
+my $envBrowser = Vim::get_view(mo_ref => $cluster_view->environmentBrowser);
+
+eval {
+	my $virtualHWs = $envBrowser->QueryConfigOptionDescriptor();
+	foreach my $vhw (@$virtualHWs) {
+		if($vhw->runSupported) {
+			print $vhw->key . " -- " . $vhw->description . "\n";
+		}
+	}
+};
+if($@) {
+	print "Error: " . $@ . "\n";
+}
+
+Util::disconnect();
