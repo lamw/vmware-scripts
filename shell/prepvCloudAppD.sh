27a50887dfe7b30fb575d1b38b138306e42d2415
William Lam <william2003@gmail.com>
2014-01-20 15:48:32 -0800

    Script to automate vCloud App Director

---
+++ shell/prepvCloudAppD.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# William lam
+# www.virtuallyghetto.com
+# Script to disable firstboot configs to allow for unattended installs & configures AppD license
+
+APPD_LICENSE=1234567
+APPD_VMDK_MOUNT_HOME=/mnt
+
+### DO NOT EDIT BEYOND HERE ###
+
+APPD_LICENSE_FILE=/home/darwin/tcserver/darwin/appd-license.properties
+APPD_FIRSTBOOT_FILE=/opt/vmware/etc/isv/firstboot
+
+MOUNTED_APPD_LICENSE_FILE=${APPD_VMDK_MOUNT_HOME}${APPD_LICENSE_FILE}
+MOUNTED_APPD_FIRSTBOOT_FILE=${APPD_VMDK_MOUNT_HOME}${APPD_FIRSTBOOT_FILE}
+
+echo "Creating AppD license file ${MOUNTED_APPD_LICENSE_FILE} ..."
+cat > ${MOUNTED_APPD_LICENSE_FILE} << __APPD_LICENSE__
+serial.number=${APPD_LICENSE}
+license.dir=/home/darwin/license
+__APPD_LICENSE__
+
+echo "Commenting out AppD firstboot script ..."
+cp "${MOUNTED_APPD_FIRSTBOOT_FILE}" "${MOUNTED_APPD_FIRSTBOOT_FILE}.bak"
+sed -i '75,102s/^/#/g' "${MOUNTED_APPD_FIRSTBOOT_FILE}"
+sed -i '287,289s/^/#/g' "${MOUNTED_APPD_FIRSTBOOT_FILE}"
