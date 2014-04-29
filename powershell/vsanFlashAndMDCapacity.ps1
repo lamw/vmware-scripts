7690174efd6749e0e1ae33bf3ecc5a0367fd74e7
wlam <wlam@htb-2s-eng-dhcp387.eng.vmware.com>
2014-04-29 09:42:19 -0700

    Scripts to report on VSAN SSD/MD capacity

---
+++ powershell/vsanFlashAndMDCapacity.ps1
@@ -0,0 +1,74 @@
+﻿# William Lam
+# www.virtuallyghetto.com
+# VSAN Flash/MD capacity report
+
+$vcName = ""
+$vcenter = Connect-VIServer $vcname -WarningAction SilentlyContinue
+
+$vsanMaxConfigInfo = @()
+
+$clusviews = Get-View -ViewType ClusterComputeResource -Property Name,ConfigurationEx,Host
+foreach ($cluster in $clusviews) {
+	if($cluster.ConfigurationEx.VsanConfigInfo.Enabled) {
+		$vmhosts = $cluster.Host
+        foreach ($vmhost in $vmhosts | Sort-Object -Property Name) {
+			$vmhostView = Get-View $vmhost -Property Name,ConfigManager.VsanSystem,ConfigManager.VsanInternalSystem	
+			$vsanSys = Get-View -Id $vmhostView.ConfigManager.VsanSystem
+			$vsanIntSys = Get-View -Id $vmhostView.ConfigManager.VsanInternalSystem
+		
+			$vsanProps = @("owner","uuid","isSsd","capacity","capacityUsed","capacityReserved")
+			$results = $vsanIntSys.QueryPhysicalVsanDisks($vsanProps)
+			$vsanStatus = $vsanSys.QueryHostStatus()
+				
+			$json = $results | ConvertFrom-Json
+			foreach ($line in $json | Get-Member) {
+				# ensure owner is owned by ESXi host
+				if($vsanStatus.NodeUuid -eq $json.$($line.Name).owner) {
+					if($json.$($line.Name).isSsd) {
+						$totalSsdCapacity += $json.$($line.Name).capacity
+						$totalSsdCapacityUsed += $json.$($line.Name).capacityUsed
+						$totalSsdCapacityReserved += $json.$($line.Name).capacityReserved
+					} else {
+						$totalMdCapacity += $json.$($line.Name).capacity
+						$totalMdCapacityUsed += $json.$($line.Name).capacityUsed
+						$totalMdCapacityReserved += $json.$($line.Name).capacityReserved
+					}				
+				}
+			}
+		}
+		$totalSsdCapacityReservedPercent = [int]($totalSsdCapacityReserved / $totalSsdCapacity * 100)
+		$totalSsdCapacityUsedPercent = [int]($totalSsdCapacityUsed / $totalSsdCapacity * 100)
+		$totalMdCapacityReservedPercent = [int]($totalMdCapacityReserved / $totalMdCapacity * 100)
+		$totalMdCapacityUsedPercent = [int]($totalMdCapacityUsed / $totalMdCapacity * 100)
+		
+		$Details = "" |Select VSANCluster, TotalSsdCapacity, TotalSsdCapacityReserved, TotalSsdCapacityUsed,TotalSsdCapacityReservedPercent, TotalSsdCapacityUsedPercent, TotalMdCapacity, TotalMdCapacityReserved, TotalMdCapacityUsed, TotalMdCapacityReservedPercent, TotalMdCapacityUsedPercent
+		$Details.VSANCluster = $cluster.Name + "`n"
+		$Details.TotalSsdCapacity = [math]::round($totalSsdCapacity /1GB,2).ToString() + " GB"
+		$Details.TotalSsdCapacityReserved = [math]::round($totalSsdCapacityReserved /1GB,2).ToString() + " GB"
+		$Details.TotalSsdCapacityUsed = [math]::round($totalSsdCapacityUsed /1GB,2).ToString() + " GB"
+		$Details.TotalSsdCapacityReservedPercent = $totalSsdCapacityReservedPercent.ToString() + "%"
+		$Details.TotalSsdCapacityUsedPercent = $totalSsdCapacityUsedPercent.ToString() + "%`n"
+		$Details.TotalMdCapacity = [math]::round($totalMdCapacity /1GB,2).ToString() + " GB"
+		$Details.TotalMdCapacityReserved = [math]::round($totalMdCapacityReserved /1GB,2).ToString() + " GB"
+		$Details.TotalMdCapacityUsed = [math]::round($totalMdCapacityUsed /1GB,2).ToString() + " GB"
+		$Details.TotalMdCapacityReservedPercent = $totalMdCapacityReservedPercent.ToString() + "%"
+		$Details.TotalMdCapacityUsedPercent = $totalMdCapacityUsedPercent.ToString() + "%"
+		$vsanMaxConfigInfo += $Details
+		
+		$totalSsdCapacity = 0
+		$totalSsdCapacityReserved = 0
+		$totalSsdCapacityUsed = 0
+		$totalSsdCapacityReservedPercent = 0
+		$totalSsdCapacityUsedPercent = 0
+		$totalMdCapacity = 0
+		$totalMdCapacityReserved = 0
+		$totalMdCapacityUsed = 0
+		$totalMdCapacityReservedPercent = 0
+		$totalMdCapacityUsedPercent = 0
+	}
+}
+
+$vsanMaxConfigInfo
+
+#Disconnect from vCenter
+Disconnect-VIServer $vcenter -Confirm:$false
---
+++ vsanFlashAndMDCapacity.pl
@@ -0,0 +1,150 @@
+#!/usr/bin/perl -w
+# Copyright (c) 2009-2014 William Lam All rights reserved.
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. The name of the author or contributors may not be used to endorse or
+#    promote products derived from this software without specific prior
+#    written permission.
+# 4. Consent from original author prior to redistribution
+
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
+# William Lam
+# www.virtuallyghetto.com
+
+use strict;
+use warnings;
+use VMware::VIRuntime;
+use VMware::VILib;
+use JSON qw(decode_json);
+
+$SIG{__DIE__} = sub{Util::disconnect()};
+
+# read and validate command-line parameters 
+Opts::parse();
+Opts::validate();
+Util::connect();
+
+my $clusters = Vim::find_entity_views(view_type => 'ComputeResource', properties => ['name','host','configurationEx']);
+my ($totalSsdCapacity,$totalSsdCapacityReserved,$totalSsdCapacityUsed,$totalMdCapacity,$totalMdCapacityReserved,$totalMdCapacityUsed) = (0,0,0,0,0,0);
+
+foreach my $cluster (@$clusters) {
+    if($cluster->{'configurationEx'}->vsanConfigInfo->enabled) {
+        my $hosts = Vim::get_views(mo_ref_array => $cluster->{'host'}, properties => ['name','configManager.vsanSystem','configManager.vsanInternalSystem','runtime.connectionState']);
+        foreach my $host(@$hosts) {
+            next if($host->{'runtime.connectionState'}->val ne 'connected');
+            # VSAN Managers
+           	my $vsanSys = Vim::get_view(mo_ref => $host->{'configManager.vsanSystem'});
+            my $vsanIntSys = Vim::get_view(mo_ref => $host->{'configManager.vsanInternalSystem'});
+            &get_vsan_disk_capacity($vsanSys,$vsanIntSys,$host);
+        }
+        my $totalSsdCapacityReservedPercent = ($totalSsdCapacityReserved / $totalSsdCapacity * 100);
+        my $totalSsdCapacityUsedPercent = ($totalSsdCapacityUsed / $totalSsdCapacity * 100);
+        my $totalMdCapacityReservedPercent = ($totalMdCapacityReserved / $totalMdCapacity * 100);
+        my $totalMdCapacityUsedPercent = ($totalMdCapacityUsed / $totalMdCapacity * 100);
+
+        print "VSAN Cluster: " . $cluster->{'name'} . "\n";
+        print "\tTotal SSD Capacity: " . &prettyPrintData($totalSsdCapacity,'B') . "\n";
+        print "\tTotal SSD Capacity Reserved: " . &prettyPrintData($totalSsdCapacityReserved,'B') . " (" . &restrict_num_decimal_digits($totalSsdCapacityReservedPercent,2) . "%)\n";
+        print "\tTotal SSD Capacity Used: " . &prettyPrintData($totalSsdCapacityUsed,'B') . " (" . &restrict_num_decimal_digits($totalSsdCapacityUsedPercent,2) . "%)\n\n";
+        print "\tTotal MD Capacity: " . &prettyPrintData($totalMdCapacity,'B') . "\n";
+        print "\tTotal MD Capacity Reserved: " . &prettyPrintData($totalMdCapacityReserved,'B') . " (" . &restrict_num_decimal_digits($totalMdCapacityReservedPercent,2) . "%)\n";
+        print "\tTotal MD Capacity Used: " . &prettyPrintData($totalMdCapacityUsed,'B') . " (" . &restrict_num_decimal_digits($totalMdCapacityUsedPercent,2) . "%)\n\n";
+
+        # zero out the global vars
+        ($totalSsdCapacity,$totalSsdCapacityReserved,$totalSsdCapacityUsed,$totalMdCapacity,$totalMdCapacityReserved,$totalMdCapacityUsed) = (0,0,0,0,0,0);
+    }
+}
+
+Util::disconnect();
+
+sub get_vsan_disk_capacity {
+	my ($vsanSys,$vsanIntSys,$host) = @_;
+
+	my $results = $vsanIntSys->QueryPhysicalVsanDisks(props => ["owner","uuid","isSsd","capacity","capacityUsed","capacityReserved"]);
+	my $vsanStatus = $vsanSys->QueryHostStatus();
+
+	# Decode JSON
+	my %decoded = %{decode_json($results)};
+
+	my $component_count = 0;
+	foreach my $key (sort keys %decoded) {
+		# ensure component is owned by ESXi host
+		if($decoded{$key}{'owner'} eq $vsanStatus->nodeUuid) {
+			if($decoded{$key}{'isSsd'}) {
+				$totalSsdCapacity += $decoded{$key}{'capacity'};
+				$totalSsdCapacityReserved += $decoded{$key}{'capacityReserved'}; 
+				$totalSsdCapacityUsed += $decoded{$key}{'capacityUsed'};
+			} else {
+				$totalMdCapacity += $decoded{$key}{'capacity'};
+				$totalMdCapacityReserved += $decoded{$key}{'capacityReserved'};
+				$totalMdCapacityUsed += $decoded{$key}{'capacityUsed'};
+			}
+		}
+	}
+}
+
+#http://www.bryantmcgill.com/Shazam_Perl_Module/Subroutines/utils_convert_bytes_to_optimal_unit.html
+sub prettyPrintData{
+    my($bytes,$type) = @_;
+
+    return '' if ($bytes eq '' || $type eq '');
+    return 0 if ($bytes <= 0);
+
+    my($size);
+
+    if($type eq 'B') {
+        $size = $bytes . ' Bytes' if ($bytes < 1024);
+        $size = sprintf("%.2f", ($bytes/1024)) . ' KB' if ($bytes >= 1024 && $bytes < 1048576);
+        $size = sprintf("%.2f", ($bytes/1048576)) . ' MB' if ($bytes >= 1048576 && $bytes < 1073741824);
+        $size = sprintf("%.2f", ($bytes/1073741824)) . ' GB' if ($bytes >= 1073741824 && $bytes < 1099511627776);
+        $size = sprintf("%.2f", ($bytes/1099511627776)) . ' TB' if ($bytes >= 1099511627776);
+    }
+    elsif($type eq 'M') {
+        $bytes = $bytes * (1048576);
+        $size = sprintf("%.2f", ($bytes/1048576)) . ' MB' if ($bytes >= 1048576 && $bytes < 1073741824);
+                $size = sprintf("%.2f", ($bytes/1073741824)) . ' GB' if ($bytes >= 1073741824 && $bytes < 1099511627776);
+                $size = sprintf("%.2f", ($bytes/1099511627776)) . ' TB' if ($bytes >= 1099511627776);
+    }
+
+    elsif($type eq 'G') {
+        $bytes = $bytes * (1073741824);
+        $size = sprintf("%.2f", ($bytes/1073741824)) . ' GB' if ($bytes >= 1073741824 && $bytes < 1099511627776);
+                $size = sprintf("%.2f", ($bytes/1099511627776)) . ' TB' if ($bytes >= 1099511627776);
+    }
+    elsif($type eq 'MHZ') {
+        $size = sprintf("%.2f", ($bytes/1e-06)) . ' MHz' if ($bytes >= 1e-06 && $bytes < 0.001);
+        $size = sprintf("%.2f", ($bytes*0.001)) . ' GHz' if ($bytes >= 0.001);
+    }
+
+    return $size;
+}
+
+# restrict the number of digits after the decimal point
+#http://guymal.com/mycode/perl_restrict_digits.shtml
+sub restrict_num_decimal_digits {
+        my $num=shift;#the number to work on
+        my $digs_to_cut=shift;# the number of digits after
+
+        if ($num=~/\d+\.(\d){$digs_to_cut,}/) {
+                $num=sprintf("%.".($digs_to_cut-1)."f", $num);
+        }
+        return $num;
+}
