20a943c38b13af472d1ed2a91b8ae1fec2d42afa
William Lam <william2003@gmail.com>
2015-04-21 21:35:25 -0700

    Script to setup VCSA 6.0 as vSphere Web Client for vSphere 5.5

---
+++ shell/setup_vcsa_as_webclient_client.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# Author: William Lam
+# Site: www.virtuallyghetto.com
+# Reference: http://www.virtuallyghetto.com/2015/04/configuring-vcsa-6-0-as-vsphere-web-client-server-for-vsphere-5-5.html 
+
+# External SSO Server
+PSC_SERVER=sso.primp-industries.com
+
+# SSO Username
+SSO_USERNAME=administrator@vsphere.local
+
+# SSO Password
+SSO_PASSWORD=VMware1!
+
+### DO NOT EDIT BEYOND HERE ###
+
+echo "Extracting vCenter Server Service ID ..."
+VC_SERVICE_ID=$(grep cmreg.serviceid $(grep vcenterserver /etc/vmware/rereg/* | awk -F ':' '{print $1}') | awk -F 'cmreg.serviceid=' '{print $2}')
+
+echo "Unregistering vCenter Server using ID: ${VC_SERVICE_ID} ..."
+/usr/lib/vmidentity/tools/scripts/lstool.py unregister --url https://${PSC_SERVER}/lookupservice/sdk --id ${VC_SERVICE_ID} --user "${SSO_USERNAME}" --password "${SSO_PASSWORD}" --no-check-cert
+
+# Stopping all services to make bootup changes
+/bin/service-control --stop --all --ignore
+
+# List of non-required services for running vSphere Web Client
+DISABLE_SERVICES=(
+applmgmt
+vmware-cis-license
+vmware-eam
+vmware-mbcs
+vmware-netdumper
+vmware-perfcharts
+vmware-rbd-watchdog
+vmware-sca
+vmware-sps
+vmware-syslog
+vmware-syslog-health
+vmware-vapi-endpoint
+vmware-vdcs
+vmware-vpx-workflow
+vmware-vpxd
+vmware-vsm
+vmware-vws
+)
+
+for SERVICE in ${DISABLE_SERVICES[@]}
+do
+        echo "Disabling ${SERVICE} and changing memory size to 0 MB ..."
+        chkconfig --force ${SERVICE} off
+        cloudvm-ram-size -C 0 ${SERVICE}
+done
+
+echo "Please shutdown the VCSA and modify its memory configuration to 3GB or as approrpiate ...""
