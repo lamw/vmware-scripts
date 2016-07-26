ac954f738d525aef29fe027eea9cf7590bbdae85
William Lam <william2003@gmail.com>
2016-07-26 07:34:14 -0700

    Script to import vCenter Server root certificate

---
+++ powershell/Import-VCRootCertificate.ps1
@@ -0,0 +1,89 @@
+# Author: William Lam
+# Blog: www.virtuallyghetto.com
+# Description: Script to import vCenter Server 6.x root certificate to Mac OS X or NIX* system
+# Reference: http://www.virtuallyghetto.com/2016/07/automating-the-import-of-vcenter-server-6-x-root-certificate.html
+
+Function Import-VCRootCertificate {
+    # Set the default download directory to current users desktop
+    # Download will be saved as cert.zip
+    $DOWNLOAD_PATH=[Environment]::GetFolderPath("Desktop")
+    $DOWNLOAD_FILE_NAME="cert.zip"
+    $DOWNLOAD_FILE_PATH="$DOWNLOAD_PATH\$DOWNLOAD_FILE_NAME"
+    $EXTRACTED_CERTS_PATH="$DOWNLOAD_PATH\certs"
+
+    # VAMI URL, easy way to verify if we have Windows VC or VCSA
+    $URL = "https://"+$VC_HOSTNAME+":5480"
+    $FOUND_VCSA = 1
+
+    # Checking to see if we have a Windows VC or VCSA
+    # as they have different SSL Certificate download endpoints
+    $websession = New-Object Microsoft.PowerShell.Commands.WebRequestSession
+    try {
+        $result = Invoke-WebRequest -Uri $URL -TimeoutSec 5
+    }
+    catch {
+        $FOUND_VCSA = 0
+    }
+
+    if($FOUND_VCSA) {
+        $VC_CERT_DOWNLOAD_URL="https://"+$VC_HOSTNAME+"/certs/download"
+    } else {
+        $VC_CERT_DOWNLOAD_URL="https://"+$VC_HOSTNAME+"/certs/download.zip"
+    }
+
+    # Required to ingore SSL Warnings
+    add-type -TypeDefinition  @"
+        using System.Net;
+        using System.Security.Cryptography.X509Certificates;
+        public class TrustAllCertsPolicy : ICertificatePolicy {
+            public bool CheckValidationResult(
+                ServicePoint srvPoint, X509Certificate certificate,
+                WebRequest request, int certificateProblem) {
+                return true;
+            }
+        }
+"@
+    [System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+
+    # Download VC's SSL Certificate
+    Write-Host "`nDownloading VC SSL Certificate to $DOWNLOAD_FILE_PATH"
+    $webclient = New-Object System.Net.WebClient
+    $webclient.DownloadFile("$VC_CERT_DOWNLOAD_URL","$DOWNLOAD_FILE_PATH")
+
+    # Extracting SSL Certificate zip file
+    Add-Type -AssemblyName System.IO.Compression.FileSystem
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($DOWNLOAD_FILE_PATH, "$DOWNLOAD_PATH")
+
+    # Find SSL certificates ending with .0
+    $Dir = get-childitem $EXTRACTED_CERTS_PATH -recurse
+    $List = $Dir | where {$_.extension -eq ".0"}
+
+    # Thanks to https://lennytech.wordpress.com/2013/06/18/powershell-install-sp-root-cert-to-trusted-root/ for snippet of code
+    # Retrieve Trusted Root Certification Store
+    $certStore = New-Object -TypeName System.Security.Cryptography.X509Certificates.X509Store Root, LocalMachine
+
+    # Import VC SSL Certificate(s) into cert store
+    Write-Host "Importing to VC SSL Certificate to Certificate Store"
+    foreach ($a in $list) {
+        $file = "$EXTRACTED_CERTS_PATH\$a"
+
+        # Get the certificate from the location where it was placed by the export process
+        $cert = New-Object -TypeName System.Security.Cryptography.X509Certificates.X509Certificate2 $file
+
+        # Open the store with maximum allowed privileges
+        $certStore.Open("MaxAllowed")
+
+        # Add the certificate to the store
+        $certStore.Add($cert)
+    }
+    # Close the store
+    $certStore.Close()
+
+    #clean up
+    Write-Host "Cleaning up, deleting $DOWNLOAD_FILE_PATH"
+    Remove-Item $DOWNLOAD_FILE_PATH
+    Write-Host "Cleaning up, deleting $EXTRACTED_CERTS_PATH"
+    Remove-Item -Recurse -Force $EXTRACTED_CERTS_PATH
+}
+
+Import-VCRootCertificate 192.168.1.51
---
+++ shell/import-vcrootcertificate.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+# Author: William Lam
+# Blog: www.virtuallyghetto.com
+# Description: Script to import vCenter Server 6.x root certificate to Mac OS X or NIX* system
+# Reference: http://www.virtuallyghetto.com/2016/07/automating-the-import-of-vcenter-server-6-x-root-certificate.html
+
+# ensure root user is running the script
+if [ "$(id -u)" != "0" ]; then
+  echo -e "Please run this script using sudo\n"
+  exit 1
+fi
+
+# Check for correct number of arguments
+if [ ${#} -ne 1 ]; then
+  echo -e "Usage: \n\t$0 [VC_HOSTNAME]\n"
+  exit 1
+fi
+
+NODE_IP=$1
+
+# Automatically determine if OS type is Mac OS X or NIX*
+if [ $(uname -s) == "Darwin" ]; then
+  OS_TYPE=OSX
+fi
+
+# Automatically determine if node is a VC or ESXi endpoint
+curl --connect-timeout 10 -k -s "https://${NODE_IP}" | grep 'forAdmins' > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+    NODE_TYPE=vc
+else
+    NODE_TYPE=esxi
+fi
+
+DOWNLOAD_PATH=/tmp/cert.zip
+if [ "${NODE_TYPE}" == "vc" ]; then
+  # Determine if VC is Windows VC or VCSA by checking VAMI endpoint
+  if [ $(curl --connect-timeout 10 -s -o /dev/null -w "%{http_code}" -i -k https://${NODE_IP}:5480) -eq 200 ]; then
+    # Install Trusted root CA for vCenter Server Appliance
+    echo -e "\nDownloading VC SSL Certificate to ${DOWNLOAD_PATH}"
+    curl -k -s "https://${NODE_IP}/certs/download" -o ${DOWNLOAD_PATH}
+    unzip ${DOWNLOAD_PATH} -d /tmp > /dev/null 2>&1
+    for i in $(ls /tmp/certs/*.0);
+    do
+      SOURCE_CERT=${i%%.*}
+      cp "${i}" "/tmp/certs/${SOURCE_CERT##*/}.crt"
+      echo "Importing to VC SSL Certificate to Certificate Store"
+      if [ ${OS_TYPE} == "OSX" ]; then
+        security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain "/tmp/certs/${SOURCE_CERT##*/}.crt"
+      else
+        cp "${i}" "/usr/local/share/ca-certificates/${SOURCE_CERT##*/}.crt"
+      fi
+    done
+  else
+    # Install Trusted root CA for vCenter Server for Windows
+    echo -e "\nDownloading VC SSL Certificate to ${DOWNLOAD_PATH}"
+    curl -k -s "https://${NODE_IP}/certs/download.zip" -o ${DOWNLOAD_PATH}
+    unzip ${DOWNLOAD_PATH} -d /tmp > /dev/null 2>&1
+    for i in $(ls /tmp/certs/*.0);
+    do
+      SOURCE_CERT=${i%%.*}
+      cp "${i}" "/tmp/certs/${SOURCE_CERT##*/}.crt"
+      echo "Importing to VC SSL Certificate to Certificate Store"
+      if [ ${OS_TYPE} == "OSX" ]; then
+        security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain "/tmp/certs/${SOURCE_CERT##*/}.crt"
+      else
+        cp /tmp/certs/lin/*.0 /usr/local/share/ca-certificates/*.crt
+        update-ca-certificates
+      fi
+    done
+  fi
+elif [ "${NODE_TYPE}" == "esxi" ]; then
+  # Install Trusted root CA for ESXi
+  echo -n | openssl s_client -showcerts -connect "${NODE_IP}":443 2>/dev/null | openssl x509 > /tmp/certs/esxi_cert.crt
+  echo "Importing to VC SSL Certificate to Certificate Store"
+  if [ ${OS_TYPE} == "OSX" ]; then
+    security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain "/tmp/certs/esxi_cert.crt"
+  else
+    cp /tmp/esxi_cert.crt /usr/local/share/ca-certificates
+    update-ca-certificates
+  fi
+fi
+echo "Cleaning up, delete /tmp/cert.zip"
+rm -rf /tmp/cert.zip
+echo "Cleaning up, delete /tmp/certs"
+rm -rf /tmp/certs
