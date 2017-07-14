0120474fbe88889390acb6a89e60e3c94889745c
William Lam <william2003@gmail.com>
2017-07-14 07:29:30 -0700

    Adding script to find SRM placholder VMs

---
+++ powershell/PlaceholderVMs.ps1
@@ -0,0 +1,25 @@
+﻿Function Get-PlaceholderVM {
+<#
+    .NOTES
+    ===========================================================================
+     Created by:    William Lam
+     Organization:  VMware
+     Blog:          www.virtuallyghetto.com
+     Twitter:       @lamw
+    ===========================================================================
+    .DESCRIPTION
+        This function retrieves all placeholder VMs that are protected by SRM
+#>
+	$results = @()
+	Foreach ($vm in Get-VM) {
+		if($vm.ExtensionData.Summary.Config.ManagedBy.Type -eq "placeholderVm") {
+			$tmp = [pscustomobject] @{
+				Name = $vm.Name;
+				ExtKey = $vm.ExtensionData.Summary.Config.ManagedBy.ExtensionKey;
+				Type = $vm.ExtensionData.Summary.Config.ManagedBy.Type
+			}
+			$results+=$tmp
+		}
+	}
+	$results
+}
