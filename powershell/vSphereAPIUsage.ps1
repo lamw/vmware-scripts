4d756675a49ffdcd0aae49d15800dd86a49e7c6d
scott1138 <30352284+scott1138@users.noreply.github.com>
2018-08-03 15:45:08 -0500

    fixed typo in $vpxdlogfile reference

--- powershell/vSphereAPIUsage.ps1
+++ powershell/vSphereAPIUsage.ps1
@@ -37,7 +37,7 @@
     }
 
     $duration = $commandDuration.TotalMinutes
-    $fileSize = [math]::Round((Get-Item -Path $vxpdLogFile).Length / 1MB,2)
+    $fileSize = [math]::Round((Get-Item -Path $vpxdLogFile).Length / 1MB,2)
     Write-host "`nFileName: $vxpdLogFile"
     Write-host "FileSize: $fileSize MB"
     Write-Host "Duration: $duration minutes"
