d434d66dfbc46bb9974fc82b55b20ab37999d740
MTBoren <technology@ddayinc.com>
2018-11-26 16:30:33 -0500

    fix credentials need that came with improvement made by removal of Client property in newer PowerCLI

--- powershell/New-xVM.ps1
+++ powershell/New-xVM.ps1
@@ -9,8 +9,9 @@ function New-xVM {
         Author:  William Lam
         Site:    www.virtuallyghetto.com
         Reference:  Blog: http://www.virtuallyghetto.com/2018/01/cross-vcenter-clone-with-vsphere-6-0.html
-        
-        Updates added Jan 2018 by Matt Boren:
+
+        Updates added by Matt Boren:
+        Jan 2018:
         - improve ease of use:  can get going with as few as four (4) parameters to function, now, vs. a dozen or more (by gleaning info from the values of the few mandatory parameters, and reduction in overall parameter count)
         - use approved verb for function name
         - add -TrustAllCert Switch parameter to give user option as to whether they want to disable SSL certificate checking, instead of disabling checking regardless
@@ -18,10 +19,10 @@ function New-xVM {
         - default to source VM name for dest VM name if no dest VM name specified
         - simplify cluster/resourcepool parameters: remove need for -Cluster, and set default value for ResourcePool, so as to reduce number of parameters needed
         - simplify specifying destination vCenter -- glean this info from the -VMHost object, instead of requiring user to additionally specify the name of the destination vCenter
-        - simplify Datacenter parameter (remove it):  glean this info from the -VMHost object, too 
+        - simplify Datacenter parameter (remove it):  glean this info from the -VMHost object, too
         - add pipeline support to take pertinent values from pipeline (SourceVM)
         - optimize array creation code (just assign collection of values to array variable, instead of potentially more resource-expensive array concatenation (which actually create a new, 1-item larger array for every item addition))
-        - get items for credential for ServiceLocatorNamePassword from destination vCenter connection object (Client.Config)
+        - get items for credential for ServiceLocatorNamePassword from destination vCenter connection object (Client.Config) -- Now unsupported (PowerCLI update removed that security loophole -- hurray for better security!)
         - improve new-object creation for use in CloneVM task (creation of fewer additional interim variables, clean up overall syntax)
         - correct logic around upper-casing the vC UUID value (was reversed)
         - improve snapshot selection (limit scope to source VM, instead of "get snapshot by name in all of source vC", which may return multiple snapshots)
@@ -29,20 +30,30 @@ function New-xVM {
         - sort out variable naming in function (had issues where global-scope variables were used inside of the function instead of the corresponding function parameters, like $destVCConn was being used instead of the parameter $destvc)
         - add -WhatIf support
         - complete the comment-based help, so that Get-Help <cmdlet> will return fully useful help (with examples and whatnot), as expected
+        Nov 2018:
+        - take destination vCenter credentials as PSCredential parameter object (previous PowerCLI / API security loophole closed -- yesss!)
+        - add switch parameter to allow for setting of newly cloned machine to template
         Ideas for other updates:
         - add support for specifying destination datastorecluster
-        - add support for cloning template
+        - get destination vCenter creds/SAML token in some way, so that user does not have to specify them (presumably already have a connection to destination vCenter, so should not be a need to specify creds for that vCenter again)
         - take objects for values for additional parameters (already did -SourceVM and -VMHost), for increased precision/accuracy (user can pass the source/destination objects directly instead of dealing with "by string", which might introduce issue due to duplicate names, etc.)
             - taking destination VPGs by object will allow for removal of -switchtype parameter, too
         - remove additional param for UppercaseUuid and just uppercase the vCenter UUID for the operation for success by default, instead of providing possiblity for failure
+        - make destination datastore, VPG parameters optional; if not specified, will just use destination datastore with most freespace and first VPG found, respectively, by default
+        - add support for cloning from template, too (not just from VM)
+        - separate -PowerOn and -MarkAsTemplate parameters into separate parametersets, so that user cannot specify both (which can lead to problematic invocations)
 
         .Example
-        Get-VM myVM -Server $mySourceVC | New-xVM -VMHost (Get-VMHost mydesthost0.dom.com -Server myDestVCenter.dom.com) -Datastore destDstore01 -VMNetwork myDestVPG0
+        Get-VM myVM -Server $mySourceVC | New-xVM -VMHost (Get-VMHost mydesthost0.dom.com -Server myDestVCenter.dom.com) -Datastore destDstore01 -VMNetwork myDestVPG0 -Credential $myDestVCCred
         Clone VM "myVM" to the vCenter of the given destination VMHost. Will name the new VM the same as the source, placing it on the given VMHost and datastore, and in the default "Resources" resource pool in the destination VMHost's parent cluster. This also takes the default for destination vSwitch type of "VDS".
 
         .Example
-        Get-VM myVM2 -Server $mySourceVC | New-xVM -VMHost (Get-VMHost mydesthost2.dom.com -Server myDestVCenter.dom.com) -Datastore destDstore02 -DestinationVMName myNewVM -VMNetwork myDestVPG1, myDestVPG2 -PowerOn:$true -TrustAllCert
+        Get-VM myVM2 -Server $mySourceVC | New-xVM -VMHost (Get-VMHost mydesthost2.dom.com -Server myDestVCenter.dom.com) -Datastore destDstore02 -DestinationVMName myNewVM -VMNetwork myDestVPG1, myDestVPG2 -PowerOn -TrustAllCert -Credential $myDestVCCred
         Clone VM to the vCenter of the given destination VMHost. Will use the new VM name, placing it on the given VMHost and datastore, and in the default "Resources" resource pool in the destination VMHost's parent cluster. This will trust SSL certificates involved (useful for when self-signed certs are in use, say, in a test environment). This also takes the default for destination vSwitch type of "VDS", connects the new VM's networks adapters to the specified VPGs, and powers on the new VM.
+
+        .Example
+        Get-Template myTemplate -Server $mySourceVC | New-xVM -VMHost (Get-VMHost mydesthost0.dom.com -Server myDestVCenter.dom.com) -Datastore destDstore01 -VMNetwork myDestVPG0 -Credential $myDestVCCred -MarkAsTemplate
+        Clone VM "myVM" to the vCenter of the given destination VMHost. Will name the new VM the same as the source, placing it on the given VMHost and datastore, and in the default "Resources" resource pool in the destination VMHost's parent cluster. This also takes the default for destination vSwitch type of "VDS". Marks as template after clone operation
 
         .Outputs
         VMware.VimAutomation.ViCore.Impl.V1.Task.TaskImpl of the Clone task. This Task object "lives" in the _source_ vCenter task list.
@@ -51,13 +62,19 @@ function New-xVM {
     [OutputType([VMware.VimAutomation.ViCore.Impl.V1.Task.TaskImpl])]
     param(
         ## The source VM object to be cloned to destination vCenter. The "Source vCenter" property will be gleaned from this object
-        [parameter(Mandatory=$true, ValueFromPipeline=$true)][VMware.VimAutomation.Types.VirtualMachine]$SourceVM,
+        [parameter(Mandatory=$true, ValueFromPipeline=$true, ParameterSetName="FromVM")][VMware.VimAutomation.Types.VirtualMachine]$SourceVM,
+
+        ## The source template object to be cloned to destination vCenter. The "Source vCenter" property will be gleaned from this object
+        [parameter(Mandatory=$true, ValueFromPipeline=$true, ParameterSetName="FromTemplate")][VMware.VimAutomation.Types.Template]$SourceTemplate,
 
         ## Name to use for new VM created in destination vCenter. If not specified, will use name of source VM
         [Alias("destvmname")][String]$DestinationVMName,
 
         ## The VMHost object from the destination vCenter on which to create the new VM
         [parameter(Mandatory=$true)][VMware.VimAutomation.Types.VMHost]$VMHost,
+
+        ## Credentials to use for API connection to destination vCenter. Could previously be gleaned from destination VMHost object, but PowerCLI security changes (for the better) no longer support such gleaning, so we need to specify these credentials here.
+        [parameter(Mandatory=$true)][System.Management.Automation.PSCredential]$CredentialForDestinationVCenter,
 
         ## Name of the destination datastore on which to create the new VM
         [parameter(Mandatory=$true)][String]$Datastore,
@@ -71,20 +88,23 @@ function New-xVM {
         ## Type of vSwitch where target VM network(s) reside on destination VMHost. One of "VDS", "VSS". Defaults to "VDS"
         [ValidateSet("VDS", "VSS")][String]$switchtype = "VDS",
 
-        ## Name(s) of VPG(s) to which to connect new VM's network adapter(s)
-        [String]$vmnetworks,
+        ## Name(s) of VPG(s) to which to connect new VM's network adapter(s). First VM network adapter will be connected to first VMNetwork name, the second to the second, and so on
+        [String[]]$VMNetwork,
 
         ## Optional: Name of snapshot on source VM from which to clone the new VM, if any
         [String]$SnapshotName,
-
-        ## Power-on the new destination VM after creation? By default, new VM will remain powered off
-        [Boolean]$poweron,
 
         ## Convert the vCenter UUID to uppercase for when performing cross vCenter operation? Some vCenters seemingly require the UUID be in uppercase
         [Boolean]$UppercaseUuid,
 
         ## Switch: Trust all SSL certificates, valid or otherwise?  Essentially, "SkipCertificateCheck". Note: this sets this behavior for the whole of the current PowerShell session, not just for this command.
-        [Alias("SkipCertificateCheck")][Switch]$TrustAllCert
+        [Alias("SkipCertificateCheck")][Switch]$TrustAllCert,
+
+        ## Switch:  Power-on the new destination VM after creation? By default, new VM will remain powered off. If marking new machine as template via -MarkAsTemplate, do not use -PowerOn, of course
+        [Switch]$PowerOn,
+
+        ## Switch:  Specifies whether or not the new virtual machine should be marked as a template. If marking new machine as template, do not use -PowerOn, of course
+        [Switch]$MarkAsTemplate
     )
 
     begin {
@@ -112,19 +132,24 @@ function New-xVM {
 
     process {
         ## get the connection object for the destination vCenter server
-        $oDestinationVIServer = $global:DefaultVIServers | Where-Object {$_.Name -eq $VMHost.Client.Config.Server}
+        $strDestinationVMHostVcenterName = ([System.Uri]$VMHost.ExtensionData.Client.ServiceUrl).DnsSafeHost
+        $oDestinationVIServer = $global:DefaultVIServers | Where-Object {$_.Name -eq $strDestinationVMHostVcenterName}
         # In the next few lines, retrieve destination VC SSL Thumbprint
-        $strDestinationVCUrl = "https://$($oDestinationVIServer.Name)"
+        $strDestinationVCUrl = "https://$strDestinationVMHostVcenterName"
         # Need to do simple GET connection for this method to work
         Invoke-RestMethod -Uri $strDestinationVCUrl -Method Get | Out-Null
         $endpoint_request = [System.Net.Webrequest]::Create($strDestinationVCUrl)
         # Get Thumbprint + add colons for a valid Thumbprint
         $destVCThumbprint = ($endpoint_request.ServicePoint.Certificate.GetCertHashString()) -replace '(..(?!$))','$1:'
 
-        # View object for source VM to clone
-        $vm_view = $SourceVM.ExtensionData
+        # View object for source machine to clone
+        $viewSourceMachine = Switch ($PSCmdlet.ParameterSetName) {
+            "FromVM" {$SourceVM.ExtensionData}
+            "FromTemplate" {$bCloneFromTemplate = $true; $SourceTemplate.ExtensionData}
+        } ## end switch
+
         ## source vCenter info (name)
-        $strSourceVCenter = $SourceVM.Client.Config.Server
+        $strSourceVCenter = ([System.Uri]$viewSourceMachine.Client.ServiceUrl).DnsSafeHost
 
         ## Get destination inventory items
         # Dest Datastore
@@ -140,8 +165,8 @@ function New-xVM {
 
         # Clone Spec
         $spec = New-Object -Type VMware.Vim.VirtualMachineCloneSpec -Property @{
-            PowerOn = $poweron
-            Template = $false
+            PowerOn = $PowerOn.ToBool()
+            Template = $MarkAsTemplate.ToBool()
         } ## end New-Object
 
         $locationSpec = New-Object -Type VMware.Vim.VirtualMachineRelocateSpec -Property @{
@@ -153,9 +178,12 @@ function New-xVM {
             service = New-Object -Type VMware.Vim.ServiceLocator -Property @{
                 credential = New-Object -Type VMware.Vim.ServiceLocatorNamePassword -Property @{
                     ## get the username and password for the destination vCenter from the VIServer connection object in the current session
-                    username = $oDestinationVIServer.Client.Config.Username
-                    password = $oDestinationVIServer.Client.Config.Password
+                    username = $CredentialForDestinationVCenter.UserName
+                    password = $CredentialForDestinationVCenter.GetNetworkCredential().Password
                 } ## end New-Object ServiceLocatorNamePassword
+                # credential = New-Object -Type VMware.Vim.ServiceLocatorSAMLCredential -Property @{
+                #     token = $oDestinationVIServer.SessionSecret
+                # } ## end New-Object ServiceLocatorSAMLCredential
                 # For some xVC-vMotion, VC's InstanceUUID must be in all caps
                 # Haven't figured out why. Hoever, this flag would allow user to toggle (default=false)
                 instanceUuid = if ($UppercaseUuid) {$oDestinationVIServer.InstanceUuid.ToUpper()} else {$oDestinationVIServer.InstanceUuid}
@@ -165,7 +193,7 @@ function New-xVM {
         } ## end New-Object VirtualMachineRelocateSpec
 
         # Find all Ethernet Devices for given VM which we will need to change its network at the destination
-        $vmNetworkAdapters = $vm_view.Config.Hardware.Device | Where-Object {$_ -is [VMware.Vim.VirtualEthernetCard]}
+        $vmNetworkAdapters = $viewSourceMachine.Config.Hardware.Device | Where-Object {$_ -is [VMware.Vim.VirtualEthernetCard]}
         # Create VM spec depending if destination networking
         # is using Distributed Virtual Switch (VDS) or
         # is using Virtual Standard Switch (VSS)
@@ -173,7 +201,7 @@ function New-xVM {
         if($switchtype -eq "vds") {
             foreach ($vmNetworkAdapter in $vmNetworkAdapters) {
                 # New VM Network to assign vNIC
-                $vmnetworkname = ($vmnetworks -split ",")[$count]
+                $vmnetworkname = $VMNetwork | Select-Object -First 1 -Skip $count
 
                 # Extract Distributed Portgroup required info
                 $dvpg = Get-VDPortgroup -Server $oDestinationVIServer -Name $vmnetworkname
@@ -196,7 +224,7 @@ function New-xVM {
         } else {
             foreach ($vmNetworkAdapter in $vmNetworkAdapters) {
                 # New VM Network to assign vNIC
-                $vmnetworkname = ($vmnetworks -split ",")[$count]
+                $vmnetworkname = $VMNetwork | Select-Object -First 1 -Skip $count
 
                 # Device Change spec for VSS portgroup
                 $dev = New-Object VMware.Vim.VirtualDeviceConfigSpec
@@ -216,12 +244,12 @@ function New-xVM {
         if($oSourceSnapshot) {$spec.Snapshot = $oSourceSnapshot.Id}
 
         $strShouldProcessMsg_Target = "VMHost '$($VMHost.Name)' in destination vCenter '$($oDestinationVIServer.Name)'"
-        $strShouldProcessMsg_Action = "Create new VM '$strDestinationVMName' from source VM '$($SourceVM.Name)' from vCenter '$strSourceVCenter'"
+        $strShouldProcessMsg_Action = "Create new {0} '$strDestinationVMName' from source {1} '$($SourceVM.Name)' from vCenter '$strSourceVCenter'" -f $(if ($MarkAsTemplate) {"template"} else {"VM"}), $(if ($bCloneFromTemplate) {"template"} else {"VM"})
         if ($PSCmdlet.ShouldProcess($strShouldProcessMsg_Target, $strShouldProcessMsg_Action)) {
-            Write-Verbose -Verbose "Cloning $($SourceVM.Name) from $strSourceVCenter to $($oDestinationVIServer.Name), creating new VM named '$strDestinationVMName'"
+            Write-Verbose -Verbose ("Cloning $($SourceVM.Name) from $strSourceVCenter to $($oDestinationVIServer.Name), creating new {0} named '$strDestinationVMName'" -f $(if ($MarkAsTemplate) {"template"} else {"VM"}))
 
-            # Issue Cross VC-vMotion, get Task MoRef back
-            $task = $vm_view.CloneVM_Task($oDestinationFolder.Id, $strDestinationVMName, $spec)
+            # Issue Cross vCenter clone, get Task MoRef back
+            $task = $viewSourceMachine.CloneVM_Task($oDestinationFolder.Id, $strDestinationVMName, $spec)
             ## return the Task object so that the user can track task as desired
             Get-Task -Server $strSourceVCenter -Id $task
         } ## end if
