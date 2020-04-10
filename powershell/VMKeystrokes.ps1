3290db1f0cbfc47d4db08c0190a31a3ad423d563
Alexis La Goutte <alexis.lagoutte@gmail.com>
2020-04-10 06:59:15 +0200

    VMKeyStrokes: fix indent (using Visual Code Formmater)

--- powershell/VMKeystrokes.ps1
+++ powershell/VMKeystrokes.ps1
@@ -23,133 +23,134 @@
         Set-VMKeystrokes -VMName $VM -StringInput "root" -DebugOn $true
 #>
     param(
-        [Parameter(Mandatory=$true)][String]$VMName,
-        [Parameter(Mandatory=$true)][String]$StringInput,
-        [Parameter(Mandatory=$false)][Boolean]$ReturnCarriage,
-        [Parameter(Mandatory=$false)][Boolean]$DebugOn
+        [Parameter(Mandatory = $true)][String]$VMName,
+        [Parameter(Mandatory = $true)][String]$StringInput,
+        [Parameter(Mandatory = $false)][Boolean]$ReturnCarriage,
+        [Parameter(Mandatory = $false)][Boolean]$DebugOn
     )
 
     # Map subset of USB HID keyboard scancodes
     # https://gist.github.com/MightyPork/6da26e382a7ad91b5496ee55fdc73db2
     $hidCharacterMap = @{
-        "a"="0x04";
-        "b"="0x05";
-        "c"="0x06";
-        "d"="0x07";
-        "e"="0x08";
-        "f"="0x09";
-        "g"="0x0a";
-        "h"="0x0b";
-        "i"="0x0c";
-        "j"="0x0d";
-        "k"="0x0e";
-        "l"="0x0f";
-        "m"="0x10";
-        "n"="0x11";
-        "o"="0x12";
-        "p"="0x13";
-        "q"="0x14";
-        "r"="0x15";
-        "s"="0x16";
-        "t"="0x17";
-        "u"="0x18";
-        "v"="0x19";
-        "w"="0x1a";
-        "x"="0x1b";
-        "y"="0x1c";
-        "z"="0x1d";
-        "1"="0x1e";
-        "2"="0x1f";
-        "3"="0x20";
-        "4"="0x21";
-        "5"="0x22";
-        "6"="0x23";
-        "7"="0x24";
-        "8"="0x25";
-        "9"="0x26";
-        "0"="0x27";
-        "!"="0x1e";
-        "@"="0x1f";
-        "#"="0x20";
-        "$"="0x21";
-        "%"="0x22";
-        "^"="0x23";
-        "&"="0x24";
-        "*"="0x25";
-        "("="0x26";
-        ")"="0x27";
-        "_"="0x2d";
-        "+"="0x2e";
-        "{"="0x2f";
-        "}"="0x30";
-        "|"="0x31";
-        ":"="0x33";
-        "`""="0x34";
-        "~"="0x35";
-        "<"="0x36";
-        ">"="0x37";
-        "?"="0x38";
-        "-"="0x2d";
-        "="="0x2e";
-        "["="0x2f";
-        "]"="0x30";
-        "\"="0x31";
-        "`;"="0x33";
-        "`'"="0x34";
-        ","="0x36";
-        "."="0x37";
-        "/"="0x38";
-        " "="0x2c";
+        "a"  = "0x04";
+        "b"  = "0x05";
+        "c"  = "0x06";
+        "d"  = "0x07";
+        "e"  = "0x08";
+        "f"  = "0x09";
+        "g"  = "0x0a";
+        "h"  = "0x0b";
+        "i"  = "0x0c";
+        "j"  = "0x0d";
+        "k"  = "0x0e";
+        "l"  = "0x0f";
+        "m"  = "0x10";
+        "n"  = "0x11";
+        "o"  = "0x12";
+        "p"  = "0x13";
+        "q"  = "0x14";
+        "r"  = "0x15";
+        "s"  = "0x16";
+        "t"  = "0x17";
+        "u"  = "0x18";
+        "v"  = "0x19";
+        "w"  = "0x1a";
+        "x"  = "0x1b";
+        "y"  = "0x1c";
+        "z"  = "0x1d";
+        "1"  = "0x1e";
+        "2"  = "0x1f";
+        "3"  = "0x20";
+        "4"  = "0x21";
+        "5"  = "0x22";
+        "6"  = "0x23";
+        "7"  = "0x24";
+        "8"  = "0x25";
+        "9"  = "0x26";
+        "0"  = "0x27";
+        "!"  = "0x1e";
+        "@"  = "0x1f";
+        "#"  = "0x20";
+        "$"  = "0x21";
+        "%"  = "0x22";
+        "^"  = "0x23";
+        "&"  = "0x24";
+        "*"  = "0x25";
+        "("  = "0x26";
+        ")"  = "0x27";
+        "_"  = "0x2d";
+        "+"  = "0x2e";
+        "{"  = "0x2f";
+        "}"  = "0x30";
+        "|"  = "0x31";
+        ":"  = "0x33";
+        "`"" = "0x34";
+        "~"  = "0x35";
+        "<"  = "0x36";
+        ">"  = "0x37";
+        "?"  = "0x38";
+        "-"  = "0x2d";
+        "="  = "0x2e";
+        "["  = "0x2f";
+        "]"  = "0x30";
+        "\"  = "0x31";
+        "`;" = "0x33";
+        "`'" = "0x34";
+        ","  = "0x36";
+        "."  = "0x37";
+        "/"  = "0x38";
+        " "  = "0x2c";
     }
 
-    $vm = Get-View -ViewType VirtualMachine -Filter @{"Name"="^$($VMName)$"}
+    $vm = Get-View -ViewType VirtualMachine -Filter @{"Name" = "^$($VMName)$" }
 
     # Verify we have a VM or fail
-    if(!$vm) {
+    if (!$vm) {
         Write-host "Unable to find VM $VMName"
         return
     }
 
     $hidCodesEvents = @()
-    foreach($character in $StringInput.ToCharArray()) {
+    foreach ($character in $StringInput.ToCharArray()) {
         # Check to see if we've mapped the character to HID code
-        if($hidCharacterMap.ContainsKey([string]$character)) {
+        if ($hidCharacterMap.ContainsKey([string]$character)) {
             $hidCode = $hidCharacterMap[[string]$character]
 
             $tmp = New-Object VMware.Vim.UsbScanCodeSpecKeyEvent
 
             # Add leftShift modifer for capital letters and/or special characters
-            if( ($character -cmatch "[A-Z]") -or ($character -match "[!|@|#|$|%|^|&|(|)|_|+|{|}|||:|~|<|>|?|*]") ) {
+            if ( ($character -cmatch "[A-Z]") -or ($character -match "[!|@|#|$|%|^|&|(|)|_|+|{|}|||:|~|<|>|?|*]") ) {
                 $modifer = New-Object Vmware.Vim.UsbScanCodeSpecModifierType
                 $modifer.LeftShift = $true
                 $tmp.Modifiers = $modifer
             }
 
             # Convert to expected HID code format
-            $hidCodeHexToInt = [Convert]::ToInt64($hidCode,"16")
+            $hidCodeHexToInt = [Convert]::ToInt64($hidCode, "16")
             $hidCodeValue = ($hidCodeHexToInt -shl 16) -bor 0007
 
             $tmp.UsbHidCode = $hidCodeValue
-            $hidCodesEvents+=$tmp
+            $hidCodesEvents += $tmp
 
-            if($DebugOn) {
+            if ($DebugOn) {
                 Write-Host "Character: $character -> HIDCode: $hidCode -> HIDCodeValue: $hidCodeValue"
             }
-        } else {
+        }
+        else {
             Write-Host "The following character `"$character`" has not been mapped, you will need to manually process this character"
             break
         }
     }
 
     # Add return carriage to the end of the string input (useful for logins or executing commands)
-    if($ReturnCarriage) {
+    if ($ReturnCarriage) {
         # Convert return carriage to HID code format
-        $hidCodeHexToInt = [Convert]::ToInt64("0x28","16")
+        $hidCodeHexToInt = [Convert]::ToInt64("0x28", "16")
         $hidCodeValue = ($hidCodeHexToInt -shl 16) + 7
 
         $tmp = New-Object VMware.Vim.UsbScanCodeSpecKeyEvent
         $tmp.UsbHidCode = $hidCodeValue
-        $hidCodesEvents+=$tmp
+        $hidCodesEvents += $tmp
     }
 
     # Call API to send keystrokes to VM
