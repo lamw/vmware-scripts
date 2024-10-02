3a5453e968ef8c5e7b00306a26a157ed09a03d3b
William Lam <wlam@vmware.com>
2024-10-02 14:59:29 -0700

    NVMe Tiering Reporting Script

---
+++ powershell/get-nvme-tiering-info.ps1
@@ -0,0 +1,39 @@
+
+$clusterName = "Cluster"
+
+$results = @()
+foreach ($vmhost in Get-Cluster -Name $clusterName | Get-VMhost | Sort-Object -Property Name) {
+    $tieringType = $vmhost.ExtensionData.Hardware.memoryTieringType
+
+    $totalMemory = [math]::round($vmhost.ExtensionData.Hardware.MemorySize /1GB,2).ToString() + " GB"
+    $tieringRatio = ($vmhost | Get-AdvancedSetting Mem.TierNvmePct).Value.toString() + "%"
+
+    $tieringEnabled = $false
+    if($tieringType -eq "softwareTiering") {
+        $tieringEnabled = $true
+
+        $dramTotal = [math]::round(($vmhost.ExtensionData.Hardware.MemoryTierInfo | where {$_.Name -eq "DRAM"}).Size /1GB,2).ToString() + " GB"
+        $nvmeTotal = [math]::round(($vmhost.ExtensionData.Hardware.MemoryTierInfo | where {$_.Name -eq "NVMe"}).Size /1GB,2).ToString() + " GB"
+
+        $storageSystem = Get-View $vmhost.ExtensionData.ConfigManager.StorageSystem
+        $nvmeDevice = ($storageSystem.StorageDeviceInfo.ScsiLun | where {$_.UsedByMemoryTiering -eq $true}).CanonicalName
+
+    } else {
+        $dramTotal = $totalMemory
+        $nvmeTotal = 0
+        $nvmeDevice = "N/A"
+    }
+
+    $tmp = [pscustomobject] @{
+        VMHost = $vmhostName
+        TieringEnabled = $tieringEnabled
+        TieringRatio = $tieringRatio
+        DRAMMemory = $dramTotal
+        NVMeMemory = $nvmeTotal
+        TotalSystemMemory = $totalMemory
+        NVMeDevice = $nvmeDevice
+    }
+    $results+=$tmp
+}
+
+$results | FT
